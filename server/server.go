b2b5a6db296276ee851e6d830b64b113d21ea609
mary <mariumkhalid267@gmail.com>
2025-10-21 04:13:02 +0100

    error handling

--- server/server.go
+++ server/server.go
@@ -13,90 +13,80 @@ type Message struct {
 	message string
 }
 
-func handleError(err error) {
-	// TODO: all
-	// Deal with an error event.
+// handleError now stops execution for fatal errors
+func handleErrorFatal(err error, context string) {
 	if err != nil {
-		fmt.Printf("Client %d disconnected\n")
-		return
+		fmt.Printf("%s: %v\n", context, err)
+		panic(err) // stop execution if fatal
 	}
+}
 
+// handleClientError handles client disconnects
+func handleClientError(err error, clientID int) bool {
+	if err != nil {
+		fmt.Printf("Client %d disconnected\n", clientID)
+		return true // signal to stop
+	}
+	return false
 }
 
 func acceptConns(ln net.Listener, conns chan net.Conn) {
-	// TODO: all
-	// Continuously accept a network connection from the Listener
-	// and add it to the channel for handling connections.
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			handleError(err)
+			fmt.Println("Failed to accept connection:", err)
+			continue
 		}
 		conns <- conn
 	}
 }
 
-func handleClient(client net.Conn, clientid int, msgs chan Message) {
-	// TODO: all
-	// So long as this connection is alive:
-	// Read in new messages as delimited by '\n's
-	// Tidy up each message and add it to the messages channel,
-	// recording which client it came from.
+func handleClient(client net.Conn, clientID int, msgs chan Message) {
 	reader := bufio.NewReader(client)
 	for {
 		msg, err := reader.ReadString('\n')
-		if err != nil {
-			handleError(err)
+		if handleClientError(err, clientID) {
+			client.Close()
+			return
 		}
-		msg = strings.TrimRight(msg, "\n") //tidy it up
-		msgs <- Message{clientid, msg}
+		msg = strings.TrimRight(msg, "\n")
+		msgs <- Message{clientID, msg}
 	}
-
 }
 
 func main() {
-	// Read in the network port we should listen on, from the commandline argument.
-	// Default to port 8030
 	portPtr := flag.String("port", ":8030", "port to listen on")
 	flag.Parse()
 
-	//TODO Create a Listener for TCP connections on the port given above.
 	ln, err := net.Listen("tcp", *portPtr)
-	if err != nil {
-		handleError(err)
-	}
+	handleErrorFatal(err, "Failed to start listener")
 
-	//Create a channel for connections
 	conns := make(chan net.Conn)
-	//Create a channel for messages
 	msgs := make(chan Message)
-	//Create a mapping of IDs to connections
 	clients := make(map[int]net.Conn)
 
-	//Start accepting connections
 	go acceptConns(ln, conns)
+
 	var nextID int
 	for {
 		select {
 		case conn := <-conns:
-			//TODO Deal with a new connection
-			// - assign a client ID
-			// - add the client to the clients map
-			// - start to asynchronously handle messages from this client
 			id := nextID
 			nextID++
 			clients[id] = conn
 			go handleClient(clients[id], id, msgs)
 
 		case msg := <-msgs:
-			//TODO Deal with a new message
-			// Send the message to all clients that aren't the sender
 			for id, conn := range clients {
 				if msg.sender != id {
-					fmt.Fprintln(conn, msg.message)
+					_, err := fmt.Fprintln(conn, msg.message)
+					if err != nil {
+						fmt.Printf("Failed to send message to client %d: %v\n", id, err)
+						conn.Close()
+						delete(clients, id)
+					}
 				}
 			}
-
 		}
 	}
 }
